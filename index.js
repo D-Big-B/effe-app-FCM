a22e887ef12a2301efea50b566613ccd91b3b5e5
D-Big-B <prjjwlbhatt@gmail.com>
2021-10-16 22:38:55 +0530

    token chaged

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const express = require("express");
-const bodyparser = require("body-parser");
 const admin = require("firebase-admin");
 
 const app = express();
-app.use(bodyparser.json());
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
-const notification_options = {
-  priority: "high",
-  timeToLive: 60 * 60 * 24,
-};
+
+// const notification_options = {
+//   priority: "high",
+//   timeToLive: 60 * 60 * 24,
+// };
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Effe-21 FCM</h1>");
 });
 
 app.post("/firebase/notification", (req, res) => {
-  const registrationToken = "general";
+  // const registrationToken = "general";
   const message = req.body.message;
-  const options = notification_options;
+  // const options = notification_options;
 
   admin
     .messaging()
-    .sendToDevice(registrationToken, message, options)
-    .then((response) => {
+    .send(message)
+    .then((res) => {
       res.status(200).send("Notification sent successfully");
     })
     .catch((error) => {
-      console.log(error);
+      res.status(400).send("Error : " + error);
     });
 });
 
